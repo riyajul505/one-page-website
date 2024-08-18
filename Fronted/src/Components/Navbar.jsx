8b2d565cacb999e2ec81cad40679f174a3571859
riyajul505 <rlriyajul@gmail.com>
2024-08-19 00:33:36 +0600

    navbar designed

--- Fronted/src/Components/Navbar.jsx
+++ Fronted/src/Components/Navbar.jsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
-    return (
-        <div>
-            
+  return (
+    <div>
+      <div className="navbar bg-base-100">
+        <div className="flex-1">
+          <a href="/" className="btn btn-ghost text-xl">OnePage</a>
         </div>
-    );
+        <div className="flex-none">
+          <button className="btn btn-square btn-ghost">
+            <NavLink to={'/login'}>Login</NavLink>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
