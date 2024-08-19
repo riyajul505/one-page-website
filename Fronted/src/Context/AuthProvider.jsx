d2083a344b146825a8b08d46103d83b22dbcbd3f
riyajul505 <rlriyajul@gmail.com>
2024-08-19 10:06:53 +0600

    setting up google login

--- Fronted/src/Context/AuthProvider.jsx
+++ Fronted/src/Context/AuthProvider.jsx
@@ -1,14 +1,20 @@
-import React, { createContext, useState } from 'react';
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { createContext, useState } from "react";
+import auth from "../Firebase/firebase.config";
 export const AuthContext = createContext(null);
-const AuthProvider = ({children}) => {
-    const [user, setUser] = useState(null);
-    const [loading, setLoading] = useState(true);
-    const drilling = {user, loading};
-    return (
-        <AuthContext.Provider value={drilling}>
-            {children}
-        </AuthContext.Provider>
-    );
+const AuthProvider = ({ children }) => {
+  const googleProvider = new GoogleAuthProvider();
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const googleLogin = () => {
+    return signInWithPopup(auth, googleProvider);
+  };
+
+  const drilling = { user, loading };
+  return (
+    <AuthContext.Provider value={drilling}>{children}</AuthContext.Provider>
+  );
 };
 
 export default AuthProvider;
